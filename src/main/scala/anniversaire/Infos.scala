66e878bc3dad7babdd9884446b52dda66d6b0964
Tiphanie Dousset <tiphdousset@gmail.com>
2019-01-06 23:15:38 +0100

    Add link to googleTranslate

--- src/main/scala/anniversaire/Infos.scala
+++ src/main/scala/anniversaire/Infos.scala
@@ -11,17 +11,18 @@ object Infos{
 
 Pour toi, cher ami.e choisis entre tous pour célébrer nos 30 ans, voici quelques informations utiles pour préparer l'évènement tant attendu.
 
-Tout d'abord la date : les festivités se dérouleront du """, b("Samedi 8 au Lundi 10 Juin"),""". Vous êtes attendus en """, b("fin d'après midi le Samedi"),""" (disons vers 17h) et nous quitterons les lieux le Lundi en fin de matinée. 
+Tout d'abord la date : les festivités se dérouleront du """, b("Samedi 8 au Lundi 10 Juin"),""". Tu es attendu en """, b("fin d'après midi le Samedi"),""" (disons vers 17h) et nous quitterons les lieux le Lundi en fin de matinée. 
 
-Pour le lieu, nous pouvons vous dire que ça se passe dans la """, b("magnifique bourgade de St Colomban (44)"),""", à 30 min au Sud de Nantes. La localisation précise du site vous sera communiquée d'ici là. 
-Nous vous conseillons de miser sur le co-voiturage depuis Nantes, qui pourra être organisé via ce site en temps et en heure. 
+Pour le lieu, nous pouvons te dire que ça se passe dans la """, b("magnifique bourgade de St Colomban (44)"),""", à 30 minutes au Sud de Nantes. La localisation précise du site te sera communiquée d'ici là. 
+Nous te conseillons de miser sur le co-voiturage depuis Nantes, qui pourra être organisé via ce site en temps et en heure. 
 Pour les non-nantais, """, b("un billet pour Nantes"),""" est à ce stade largement suffisant.
 
-Pour le couchage, un champ sera mis à disposition pour planter """, b("les tentes"),""".  Pensez donc à prendre votre tente ;)
+Pour le couchage, un champ sera mis à disposition pour planter """, b("les tentes"),""".  Pense donc à prendre ta tente ;-)
 
 Si certains d'entre vous souhaitent arriver plus tôt / partir plus tard, n'hésitez pas à nous contacter afin que l'on puisse vous accueillir chez l'un d'entre nous.
 
-Non-french speaker : keep calm and don't panic, you can ask us for more explanations or...""",
+Non-french speaker : keep calm and don't panic, you can ask us for more explanations """,
+a(href := "https://translate.google.de/#view=home&op=translate&sl=fr&tl=en", "or"),""" """,
     
     fontSize := "20px", whiteSpace := "pre-line", fontFamily := "montserrat")
 
