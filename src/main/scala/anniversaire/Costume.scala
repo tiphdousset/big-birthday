d0f6b4e816780b4804cefdff24f4e402fe803f62
tiphanie <tiphanie.dousset@hivemindtechnologies.com>
2019-05-13 22:53:11 +0200

    Use mp4 instead of iframe

--- src/main/scala/anniversaire/Costume.scala
+++ src/main/scala/anniversaire/Costume.scala
@@ -5,6 +5,8 @@ import rx._
 import util._
 import monix.reactive._
 import TokenLogic._
+import org.scalajs.dom.raw.HTMLIFrameElement
+
 import concurrent.duration._
 import org.scalajs.dom.window.setTimeout
 
@@ -12,7 +14,7 @@ object Costume {
 
   def costume(token: String, language: Observable[Translation])(
       implicit ctx: Ctx.Owner) = {
-//     val showWheel = Var(false)
+
     val showCostume = Var(1)
 
     val button_wheel = button(
@@ -22,49 +24,54 @@ object Costume {
       marginTop := "50px",
       marginLeft := "auto",
       marginRight := "auto",
-      backgroundColor:="#fd22c4",
-      color:="white",
+      backgroundColor := "#fd22c4",
+      color := "white",
       display := "block",
       onClick(2) --> showCostume,
-      onClick.foreach{ _ =>
-        setTimeout(() => showCostume()=3,3000.0)
-      }
     )
 
     div(
       language.map(_.menu_costume),
       Rx {
-        if (showCostume()==2)
-          wheelOfFortune
-        else if (showCostume()==3)
+        if (showCostume() == 2)
+          magicGif(showCostume)
+        else if (showCostume() == 3)
           displayCostume(token, language)
         else
           button_wheel
       },
       fontSize := "16px",
-      whiteSpace := "pre-line"
+      whiteSpace := "pre-line",
     )
   }
 
   def displayCostume(token: String, language: Observable[Translation])(
       implicit ctx: Ctx.Owner) = {
     val guestName = TokenLogic.findNameAndCostumePerToken(token).toList(0)._1
-    println("guestName = "+guestName)
+    println("guestName = " + guestName)
     val costume = TokenLogic.findNameAndCostumePerToken(token).toList(0)._2
-    println("costume = "+costume)
+    println("costume = " + costume)
     val costumePartner = TokenLogic.findCostumePartner(costume)
-    println("costumePartner = "+ costumePartner)
-    div(language.map(_.display_costume(guestName, costume, costumePartner)), marginTop:="20px", display.flex, flexDirection:="column", alignItems:="center")
+    println("costumePartner = " + costumePartner)
+    div(language.map(_.display_costume(guestName, costume, costumePartner)),
+        marginTop := "20px",
+        display.flex,
+        flexDirection := "column",
+        alignItems := "center")
   }
-   val wheelOfFortune = div(iframe( src := "https://giphy.com/embed/6CovzgyTig7M4",
-     attr("width") := "480",
-     attr("height") := "360",
-     attr("frame-border") := "0",
-     cls := "giphy-embed",
-     borderWidth := "0",
-     display := "block",
-     marginLeft := "auto",
-     marginRight := "auto"
-     ) )
+  def magicGif(showCostume: Var[Int]) =
+    div(
+      display:="flex",
+      video(
+        margin:="20px auto",
+        source(src := "https://media.giphy.com/media/6CovzgyTig7M4/giphy.mp4",
+               tpe := "video/mp4"),
+        attr("autoplay") := true,
+        onDomMount.asHtml.foreach { x =>
+          x.onended = { _ =>
+            showCostume() = 3
+          }
+        }
+      ))
 
 }
