349c50dacfa008c923b10f6f98b4286990cfd245
Tiphanie <tiphanie.dousset@hivemindtechnologies.com>
2025-03-27 18:45:04 +0100

    Update countdown to new date

--- webapp/src/main/scala/webapp/anniversaire/CountDown.scala
+++ webapp/src/main/scala/webapp/anniversaire/CountDown.scala
@@ -7,14 +7,15 @@ import java.util.Date
 object CountDown {
 
   def countDown(language: Translation) = {
-    val eventDate = new Date(2019 - 1900, 5, 8) // 0 is January so 5 is June
+    val eventDate = new Date(2025 - 1900, 5, 7) // 0 is January so 5 is June
     val today = new Date()
-    val remainingDays = (((eventDate.getTime() - today.getTime()) / 1000) / (24 * 60 * 60) + 1).toInt
+    val remainingDays = (((eventDate.getTime() - today
+      .getTime()) / 1000) / (24 * 60 * 60) + 1).toInt
 
     remainingDays match {
-      case days if days > 0 => language.count_down(remainingDays.toString)
+      case days if days > 0  => language.count_down(remainingDays.toString)
       case days if days > -2 => language.count_downNow
-      case _ => language.count_downAfter
+      case _                 => language.count_downAfter
     }
   }
 }
